02f5b1ce8074cfda305a559184d2387975805bf8
jc1518 <support@jackiechen.org>
2014-09-02 11:13:02 +1000

    sys_motd, sys_ldap works in lab

---
+++ puppet/trunk/environments/production/manifests/site.pp
@@ -0,0 +1,4 @@
+node default {
+  include sys_motd
+  include sys_ldap
+}
---
+++ puppet/trunk/environments/production/modules/sys_ldap/manifests/init.pp
@@ -0,0 +1,39 @@
+class sys_ldap {
+  
+  package { 'openldap':
+    ensure => installed,
+  }  
+
+  package { 'openldap-clients':
+    ensure => installed,
+    require => Package['openldap']
+  }
+  
+  package { 'sssd':
+    ensure => installed,
+  }
+
+  file { '/etc/openldap/ldap.conf':
+    ensure => file,
+    source => 'puppet:///sys_ldap/ldap.conf',
+    require => Package['openldap-clients'],
+  }
+
+  file { '/etc/openldap/certs/ldap-pub.pem':
+     ensure => file,
+     source => 'puppet:///sys_ldap/ldap-pub.pem',
+     require => Package['openldap-clients'],
+  }
+
+  file { '/etc/sssd/sssd.conf':
+    ensure => file,
+    source => 'puppet:///sys_ldap/sssd.conf',
+    mode => 600,
+    require => Package['sssd'],
+  }
+
+  exec { '/usr/sbin/authconfig --enablesssd --enablesssdauth --enableldap --enableldapauth --enablemkhomedir --ldapserver=ldaps://ldap.mylab.local --ldapbasedn=dc=mylab,dc=local --enablelocauthorize --enableldaptls --update':
+    require => [ File['/etc/openldap/ldap.conf'], File['/etc/openldap/certs/ldap-pub.pem'], File['/etc/sssd/sssd.conf'], ],
+  }
+
+}
